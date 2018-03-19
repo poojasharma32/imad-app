1bed666d11bd8fab928900f928753a8a85cdd54f
poojasharma32 <37449770+poojasharma32@users.noreply.github.com>
2018-03-19 21:53:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', '146.png'));
 });
 //mypracticecode//
 app.get('article1',function(req,res)
